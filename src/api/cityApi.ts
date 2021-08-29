3fb4d70a0aa43a26ed945d89f7bc8ccb4fd99170
Son Nguyen <nhuson2306@gmail.com>
2021-08-29 23:26:56 +0700

    Setup APIs for city resource in reactjs typescript

--- src/api/cityApi.ts
+++ src/api/cityApi.ts
@@ -1,10 +1,16 @@
+import { City, ListResponse } from 'models';
 import axiosClient from './axiosClient';
 
 const cityApi = {
-  getAllCities() {
+  getAllCities(): Promise<ListResponse<City>> {
     const url = '/cities';
 
-    return axiosClient.get(url);
+    return axiosClient.get(url, {
+      params: {
+        _page: 1,
+        _limit: 10,
+      },
+    });
   },
 };
 
